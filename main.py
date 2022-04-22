a6deec15d19725980392284de55d8f78195b88a6
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2022-04-22 11:37:59 +0200

    Two instances communicating on one machine

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import socket
-import time
 import threading
 
 from server import Server
 from client import Client
 
 
-port = 8080
-serv = Server(port)
-clie = Client(socket.gethostname(), port)
+# TODO: pobieranie adresów z bazy
+receive_port = int((input("Podaj port odbierania: ")))
+dest_port = int(input("Podaj port docelowy: "))
+
+serv = Server(receive_port)
+clie = Client(socket.gethostname(), dest_port)
 
 server_thread = threading.Thread(target=serv.start)
 server_thread.start()
 
+input("Press enter to connect\n")
 clie.start()  # main thread in the client
 
 serv.stop()
