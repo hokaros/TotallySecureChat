48af32c9e39cfd551ce68197500d2dc5b504966a
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2022-06-04 11:54:39 +0200

    login screen

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from server import Server
 from client import Client
 from gui_window import ChatWindow
 from message import Message
+from gui_login import LoginWindow
 
 
 def receive_message(msg: Message):
@@ -14,10 +15,26 @@ def receive_message(msg: Message):
     window.receive_message(msg.stringbody(), msg.sender_id)
 
 
-receive_port = int((input("Receiving port: ")))
-user_id = receive_port
+credentials = {"_PORT_": None, "_RECEIVER_PORT_": None, "_PASSWORD_": None}
 
-password = input("Password: ")
+
+def login(input: dict):
+    global credentials
+    for key in input:
+        credentials[key] = input[key]
+
+
+login_window = LoginWindow()
+login_window.subscribe_confirm(login)
+
+login_window.run()
+login_window.close()
+
+receive_port = int(credentials["_PORT_"])
+user_id = receive_port
+dest_port = int(credentials["_RECEIVER_PORT_"])
+
+password = credentials["_PASSWORD_"]
 
 serv = Server(receive_port, user_id, password)
 serv.subscribe_message_received(receive_message)
@@ -26,9 +43,7 @@ server_thread = threading.Thread(target=serv.start)
 server_thread.start()
 
 time.sleep(0.1)
-dest_port = int(input("Target port: "))
 clie = Client(user_id, socket.gethostname(), dest_port, password)
-input("Press enter to connect\n")
 clie.start()
 
 # Run GUI
@@ -38,9 +53,7 @@ window.subscribe_message_send(clie.send_text)
 window.run()
 window.close()
 
-
 # Close connections
 clie.stop()
 serv.stop()
 print("Stopped all services")
-
